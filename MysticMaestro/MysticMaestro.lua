2e16d3462f25d28cc7eb05b254c484c48e7c2020
BanditTech <thebbandit@gmail.com>
2022-06-13 20:32:26 -0700

    Try making a GUI

--- MysticMaestro/MysticMaestro.lua
+++ MysticMaestro/MysticMaestro.lua
@@ -2,3 +2,46 @@ local addonName, addonTable = ...
 
 local AceAddon = LibStub("AceAddon-3.0")
 local MM = AceAddon:NewAddon("MysticMaestro", "AceConsole-3.0")
+local AceGUI = LibStub("AceGUI-3.0")
+local Dialog = LibStub("AceConfigDialog-3.0")
+
+local myOptionsTable = {
+	name = "Mystic Maestro",
+	handler = MM,
+  type = "group",
+  args = {
+    enable = {
+      name = "Enable",
+      desc = "Enables / disables the addon",
+      type = "toggle",
+      set = function(info,val) MM.enabled = val end,
+      get = function(info) return MM.enabled end
+    } --,
+    -- moreoptions={
+    --   name = "More Options",
+    --   type = "group",
+    --   args={
+    --     -- more options go here
+    --   }
+    -- }
+  }
+}
+
+function MM:OpenMenu()
+	if UnitAffectingCombat("player") then
+			if Dialog.OpenFrames["Mystic Maestro"] then
+					Dialog:Close("Mystic Maestro")
+			end
+			return
+	end
+
+	if Dialog.OpenFrames["Mystic Maestro"] then
+			Dialog:Close("Mystic Maestro")
+	else
+			Dialog:Open("Mystic Maestro")
+	end
+end
+
+LibStub("AceConfig-3.0"):RegisterOptionsTable("Mystic Maestro",myOptionsTable)
+
+MM:RegisterChatCommand("mm","mystic","maestro")
