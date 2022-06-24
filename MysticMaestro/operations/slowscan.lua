066258d466cd7cc8c64988f8ede5b22adc5d9647
BanditTech <thebbandit@gmail.com>
2022-06-24 12:56:57 -0700

    add slow scan file

---
+++ MysticMaestro/operations/slowscan.lua
@@ -0,0 +1,10 @@
+﻿local MM = LibStub("AceAddon-3.0"):GetAddon("MysticMaestro")
+
+function MM:HandleSlowScan()
+	local AuctionFrame = _G["AuctionFrame"]
+	if AuctionFrame and AuctionFrame:IsShown() then
+		MM:Print("Auction house window is open and we did a slow scan! ;)")
+	else
+		MM:Print("Auction house window must be open to perform scan")
+	end
+end
