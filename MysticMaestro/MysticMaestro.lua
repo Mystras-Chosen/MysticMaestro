a0444a7d82c0c27951c9b49aef5fc54b917c74d9
BanditTech <thebbandit@gmail.com>
2022-06-28 00:52:11 -0700

    move tooltip functions into their file

--- MysticMaestro/MysticMaestro.lua
+++ MysticMaestro/MysticMaestro.lua
@@ -103,27 +103,3 @@ function MM:ProcessSlashCommand(input)
 end
 
 MM:RegisterChatCommand("mm", "ProcessSlashCommand")
-
-function MM:TooltipHandler(tooltip, ...)
-  local enchantName = MM:MatchTooltipRE(tooltip)
-  if enchantName then
-    local enchantStats = self.db.realm.RE_AH_STATISTICS[enchantName]["current"]
-    tooltip:AddDoubleLine("Mystic Maestro", enchantName)
-    if enchantStats ~= nil then
-      local ttMin = enchantStats.minVal / 10000 or 0.0
-      local ttMed = enchantStats.medVal / 10000 or 0.0
-      local ttAvg = enchantStats.avgVal / 10000 or 0.0
-      tooltip:AddDoubleLine("Number Listed", enchantStats.listed or 0.0)
-      tooltip:AddDoubleLine("Min/Med/Avg", "("..ttMin.."/"..ttMed.."/"..ttAvg..")")
-    else
-      tooltip:AddDoubleLine("Number Listed", "none found")
-    end
-  end
-end
-
-GameTooltip:HookScript(
-  "OnTooltipSetItem",
-  function(...)
-    MM:TooltipHandler(...)
-  end
-)
---
+++ MysticMaestro/utils/tooltip.lua
@@ -0,0 +1,25 @@
+﻿local MM = LibStub("AceAddon-3.0"):GetAddon("MysticMaestro")
+
+function MM:TooltipHandler(tooltip, ...)
+  local enchantName = MM:MatchTooltipRE(tooltip)
+  if enchantName then
+    local enchantStats = self.db.realm.RE_AH_STATISTICS[enchantName]["current"]
+    tooltip:AddDoubleLine("Mystic Maestro", enchantName)
+    if enchantStats ~= nil then
+      local ttMin = tonumber(enchantStats.minVal) / 10000 or 0.0
+      local ttMed = tonumber(enchantStats.medVal) / 10000 or 0.0
+      local ttAvg = tonumber(enchantStats.avgVal) / 10000 or 0.0
+      tooltip:AddDoubleLine("Number Listed", enchantStats.listed or 0.0)
+      tooltip:AddDoubleLine("Min/Med/Avg", "("..ttMin.."/"..ttMed.."/"..ttAvg..")")
+    else
+      tooltip:AddDoubleLine("Number Listed", "none found")
+    end
+  end
+end
+
+GameTooltip:HookScript(
+  "OnTooltipSetItem",
+  function(...)
+    MM:TooltipHandler(...)
+  end
+)
