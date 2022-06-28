15ca50d3ef4569db782babe8247f3c59e82ac78b
BanditTech <thebbandit@gmail.com>
2022-06-28 00:26:54 -0700

    tooltips

--- MysticMaestro/MysticMaestro.lua
+++ MysticMaestro/MysticMaestro.lua
@@ -110,11 +110,13 @@ function MM:TooltipHandler(tooltip, ...)
     local enchantStats = self.db.realm.RE_AH_STATISTICS[enchantName]["current"]
     tooltip:AddDoubleLine("Mystic Maestro", enchantName)
     if enchantStats ~= nil then
-      local ttMin = enchantStats.minVal or 0.0
-      local ttMed = enchantStats.medVal or 0.0
-      local ttAvg = enchantStats.avgVal or 0.0
+      local ttMin = enchantStats.minVal / 10000 or 0.0
+      local ttMed = enchantStats.medVal / 10000 or 0.0
+      local ttAvg = enchantStats.avgVal / 10000 or 0.0
       tooltip:AddDoubleLine("Number Listed", enchantStats.listed or 0.0)
       tooltip:AddDoubleLine("Min/Med/Avg", "("..ttMin.."/"..ttMed.."/"..ttAvg..")")
+    else
+      tooltip:AddDoubleLine("Number Listed", "none found")
     end
   end
 end
