6d062b6b895cacc8e551d9e1a257f2419c4b2db1
BanditTech <thebbandit@gmail.com>
2022-07-18 23:06:12 -0700

    add unlock message from REToolbox

--- MysticMaestro/MysticMaestro.lua
+++ MysticMaestro/MysticMaestro.lua
@@ -54,6 +54,7 @@ function MM:OnInitialize()
   self.db.realm.RE_AH_LISTINGS = setmetatable(self.db.realm.RE_AH_LISTINGS or {}, enchantMT)
   self.db.realm.RE_AH_STATISTICS = setmetatable(self.db.realm.RE_AH_STATISTICS or {}, enchantMT)
   self.db.realm.FAVORITE_ENCHANTS = self.db.realm.FAVORITE_ENCHANTS or {}
+  MM:RegisterMessage("ASC_COLLECTION_RE_UNLOCKED")
 end
 
 function MM:OnEnable()
