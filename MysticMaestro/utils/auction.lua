57a60b22b8a5442e8752262442dd5de27c3ec47d
BanditTech <thebbandit@gmail.com>
2022-11-05 15:00:37 -0700

    add logic to findSellableItemWithEnchantID

--- MysticMaestro/utils/auction.lua
+++ MysticMaestro/utils/auction.lua
@@ -446,17 +446,42 @@ StaticPopupDialogs["MM_LIST_AUCTION"] = {
   enterClicksFirstButton = 1  -- doesn't cause taint for some reason
 }
 
+local cTip = CreateFrame("GameTooltip","MMScanTip",nil,"GameTooltipTemplate")
+local function IsSoulbound(bag, slot)
+    cTip:SetOwner(UIParent, "ANCHOR_NONE")
+    cTip:SetBagItem(bag, slot)
+    cTip:Show()
+    for i = 1,cTip:NumLines() do
+        if(_G["MMScanTipTooltipTextLeft"..i]:GetText()==ITEM_SOULBOUND) then
+            -- cTip:Hide()
+            return true
+        end
+    end
+    -- cTip:Hide()
+    return false
+end
+
 -- only do trinkets for now, and return nil if trinket with enchantID not found
 local function findSellableItemWithEnchantID(enchantID)
-  -- for bagID=0, 4 do
-  --   for slotIndex=1, GetContainerNumSlots(bagID) or 0 do
-  --     -- not soulbound, is rare or higher quality, and istrinket with the specified enchantID on it
-  --     if true then
-  --       return bagID, slotIndex
-  --     end
-  --   end
-  -- end
-  return 0, 1
+  local items = {}
+  items.trinket = {}
+  items.other = {}
+  for bagID=0, 4 do
+    for slotIndex=1, GetContainerNumSlots(bagID) or 0 do
+      -- not soulbound, is rare or higher quality, and istrinket with the specified enchantID on it
+      local _,_,_,quality,_,_,item = GetContainerItemInfo(i, j)
+      if item and quality >= 3 and not IsSoulbound(bagID, slotIndex) then
+        local re = GetREInSlot(bagID, slotIndex)
+        if re == enchantID then
+          local istrinket = item:find("Insignia of the")
+          table.insert(istrinket and items.trinket or items.other, {bagID=bagID, slotIndex=slotIndex})
+        end
+      end
+    end
+  end
+  if #items.trinket then
+    return unpack(items.trinket[1])
+  end
 end
 
 
