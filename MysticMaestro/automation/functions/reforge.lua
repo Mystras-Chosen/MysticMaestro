1c0851e102470ed27011707f3b8987f63473ff9f
BanditTech <thebbandit@gmail.com>
2023-01-26 01:19:55 -0800

    init reforge file -- still refactoring

---
+++ MysticMaestro/automation/functions/reforge.lua
@@ -0,0 +1,304 @@
+﻿local MM = LibStub("AceAddon-3.0"):GetAddon("MysticMaestro")
+
+local automationName = "Reforge"
+
+local isPaused
+
+local automationTable = {}
+
+function automationTable.GetName()
+  return automationName
+end
+
+local options
+
+function automationTable.ShowInitPrompt()
+  options = options or MM.db.realm.OPTIONS
+  MM.AutomationUtil.ShowAutomationPopup(automationName, automationTable, "prompt")
+end
+
+local running
+
+function automationTable.Start()
+end
+
+function automationTable.Pause()
+  if running then
+    isPaused = true
+    MM.AutomationUtil.HideAutomationPopup()
+  elseif isPaused then -- can be called when already paused and init prompt showing
+    MM.AutomationUtil.HideAutomationPopup()
+  else
+    MM:Print("ERROR: " .. automationName .." paused when not running")
+  end
+end
+
+function automationTable.IsPaused()
+  return isPaused
+end
+
+function automationTable.Stop()
+  MM.AutomationUtil.HideAutomationPopup()
+  isPaused = false
+  running = false
+end
+
+MM.AutomationManager:RegisterAutomation(automationName, automationTable)
+
+-- Refactored code below
+
+local itemLoaded = false
+local bagID, slotIndex, autoAutoEnabled, autoReforgeEnabled
+local enchantsOfInterest
+local reforgeHandle
+
+-- Options to configure:
+-- stopForShop.enabled, stopForShop.unknown, shoppingLists
+-- stopSeasonal.enabled, stopSeasonal.extract
+-- stopSeasonal.enabled, stopSeasonal.extract
+-- stopQuality.enabled, options.stopQuality[tostring(currentEnchant.quality)]
+-- stopUnknown.enabled, stopUnknown.extract, stopUnknown[tostring(currentEnchant.quality)]
+-- stopIfNoRunes
+-- stopPrice.enabled, stopPrice.value, stopPrice[tostring(currentEnchant.quality)]
+
+if not MysticMaestroEnchantingFrameAutoReforgeButton then
+	local button = CreateFrame("Button", "MysticMaestroEnchantingFrameAutoReforgeButton", MysticEnchantingFrame, "UIPanelButtonTemplate")
+	button:SetWidth(80)
+	button:SetHeight(22)
+	button:SetPoint("BOTTOMLEFT", 300, 36)
+	button:Disable()
+	button:RegisterForClicks("AnyUp")
+	button:SetScript("OnClick", function(self)
+		if itemLoaded then
+			if self:GetText() == "Auto Reforge" then
+				StartAutoReforge()
+				RequestReforge()
+			else
+				StopAutoReforge()
+			end
+		else
+			if self:GetText() == "Auto Reforge" then
+				StartAutoAutoReforge()
+			else
+				StopAutoAutoReforge()
+			end
+		end
+	end)
+	button:SetText("Auto Reforge")
+	MysticEnchantingFrameControlFrameRollButton:HookScript("OnEnable", function() itemLoaded = true end )
+	MysticEnchantingFrameControlFrameRollButton:HookScript("OnDisable", function() itemLoaded = false end )
+end
+
+local function BuildWorkingShopList()
+	local shopList = {}
+	for _, list in ipairs(options.shoppingLists) do
+		for _, enchantName in pairs(list) do
+			if _ ~= "name" and enchantName ~= "" then
+				local n = enchantName:lower()
+				shopList[select(3, n:find("%[(.-)%]")) or select(3, n:find("(.+)"))] = true
+			end
+		end
+	end
+	enchantsOfInterest = shopList
+end
+
+if options.stopForShop.enabled then
+	BuildWorkingShopList()
+end
+
+
+local colorHex = {
+    ["green"] = "|cff00ff00",
+    ["red"] = "|cffff0000"
+}
+
+local function configShoppingMatch(currentEnchant)
+    return options.stopForShop.enabled and enchantsOfInterest[currentEnchant.spellName:lower()] 
+    and (not options.stopForShop.unknown or (options.stopForShop.unknown and not IsReforgeEnchantmentKnown(currentEnchant.enchantID)))
+end
+
+local function configSeasonalMatch(currentEnchant)
+    local eval = options.stopSeasonal.enabled and isSeasonal(currentEnchant.enchantID)
+    if eval and options.stopSeasonal.extract then
+        -- Code for extraction
+        extract(currentEnchant.enchantID)
+    end
+    return eval
+end
+
+local function configQualityMatch(currentEnchant)
+    return options.stopQuality.enabled and options.stopQuality[tostring(currentEnchant.quality)]
+end
+
+local function configUnknownMatch(currentEnchant)
+    local eval = options.stopUnknown.enabled and not IsReforgeEnchantmentKnown(currentEnchant.enchantID) and options.stopUnknown[tostring(currentEnchant.quality)]
+    if eval and options.stopUnknown.extract then
+        -- Code for extraction
+        extract(currentEnchant.enchantID)
+    end
+    return eval
+end
+
+local function configNoRunes(currentEnchant)
+    return options.stopIfNoRunes and GetItemCount(98462) <= 0
+end
+
+local function configPriceMatch(currentEnchant)
+    local priceObj = Maestro(currentEnchant.enchantID)
+    if not priceObj then return options.stopPrice[tostring(currentEnchant.quality)] end
+    return options.stopPrice.enabled and priceObj.Min >= options.stopPrice.value * 10000 and options.stopPrice[tostring(currentEnchant.quality)]
+end
+
+local function configConditionMet(currentEnchant)
+    return configNoRunes(currentEnchant)
+    or configQualityMatch(currentEnchant)
+    or configShoppingMatch(currentEnchant)
+    or configUnknownMatch(currentEnchant)
+    or configSeasonalMatch(currentEnchant)
+    or configPriceMatch(currentEnchant)
+end
+
+local function getLinkRE(ID)
+    local RE = MYSTIC_ENCHANTS[ID]
+    local color = AscensionUI.MysticEnchant.EnchantQualitySettings[RE.quality][1]
+    return color .. "\124Hspell:" .. RE.spellID .. "\124h[" .. RE.spellName .. "]\124h\124r"
+end
+
+local function isSeasonal(spellID)
+    local enchant = GetMysticEnchantInfo(spellID)
+    if enchant then
+        return not bit.contains(enchant.realms, Enum.RealmMask.Area52)
+    end
+end
+
+local function extract(enchantID)
+    if not IsReforgeEnchantmentKnown(enchantID) 
+    and GetItemCount(98463) and (GetItemCount(98463) > 0) then
+        print("Extracting enchant:" .. getLinkRE(enchantID))
+        RequestSlotReforgeExtraction(bagID, slotIndex)
+    end
+end
+
+-- Trigger 1 : COMMENTATOR_SKIRMISH_QUEUE_REQUEST
+local function COMMENTATOR_SKIRMISH_QUEUE_REQUEST(event, subEvent, sourceGUID, enchantID)
+	if subEvent ~= "ASCENSION_REFORGE_ENCHANT_RESULT" then return end
+	
+	if tonumber(sourceGUID) == tonumber(UnitGUID("player"), 16) then
+		local currentEnchant = MYSTIC_ENCHANTS[enchantID]
+		if not autoAutoEnabled and (not autoReforgeEnabled or configConditionMet(currentEnchant)) then
+			-- End reforge
+			StopAutoReforge()
+			return
+		end
+		
+		if autoAutoEnabled then
+			local knownStr, seasonal = "known", ""
+			if not IsReforgeEnchantmentKnown(enchantID) then
+				knownStr = colorHex.red .. "un" .. knownStr .. "|r"
+			else
+				knownStr = colorHex.green .. knownStr .. "|r"
+			end
+			if isSeasonal(enchantID) then
+				seasonal = colorHex.green .. " seasonal" .. "|r"
+			end
+			
+			if configConditionMet(currentEnchant) then
+				print("Stopped on " .. knownStr .. seasonal .. " enchant:" .. getLinkRE(enchantID))
+
+				if not findNextInsignia() or GetItemCount(98462) <= 0 then
+					if GetItemCount(98462) <= 0 then
+						print("Out of runes")
+					else
+						print("Out of Insignia")
+					end
+					WeakAuras.ScanEvents("POLI_AUTO_AUTO_OFF")
+					return
+				end
+			else
+				print("Skipping " .. knownStr .. seasonal .. " enchant:" .. getLinkRE(enchantID))
+			end
+		end
+		RequestReforge()
+	end
+end
+
+-- Trigger 2 : POLI_REFORGE_REQUESTED, POLI_REFORGE_REQUEST_TERMINATED, UNIT_SPELLCAST_START, UNIT_SPELLCAST_STOP
+local function UNIT_SPELLCAST_START(event, unitID, spell)\
+	if event ~= "UNIT_SPELLCAST_START" then return end
+	-- if cast has started, then stop trying to cast
+	if unitID == "player" and spell == "Enchanting" then
+		StopCraftingAttemptTimer()
+	end
+end
+
+local function RequestReforge()
+	-- attempt to roll every .05 seconds
+	if autoReforgeEnabled then
+		reforgeHandle = Timer.NewTicker(.05, function()
+			MysticEnchantingFrameControlFrameRollButton:GetScript("OnClick")(MysticEnchantingFrameControlFrameRollButton)
+		end)
+	elseif autoAutoEnabled then
+		reforgeHandle = Timer.NewTicker(.05, function()
+			RequestSlotReforgeEnchantment(bagID, slotIndex)
+		end)
+	else
+			print("Error starting reforge, values indicate we are not enabled. AR:" .. autoReforgeEnabled .. " AA:" .. autoAutoEnabled)
+	end
+end
+
+local function StopCraftingAttemptTimer()
+	if reforgeHandle then
+		reforgeHandle:Cancel()
+		reforgeHandle = nil
+	end
+end
+
+local function ThreeDotsString()
+	local floorTime = math.floor(GetTime())
+	return floorTime % 3 == 0 and "." or (floorTime % 3 == 1 and ".." or "...")
+end
+
+-- Trigger 3 : POLI_AUTO_REFORGE_ON, POLI_AUTO_REFORGE_OFF
+local function StartAutoReforge()
+	autoReforgeEnabled = true
+	MysticMaestroEnchantingFrameAutoReforgeButton:SetText("Reforging" .. ThreeDotsString())
+	dynamicButtonTextHandle = Timer.NewTicker(1, function()
+		MysticMaestroEnchantingFrameAutoReforgeButton:SetText("Reforging" .. ThreeDotsString())
+	end)
+end
+
+local function StopAutoReforge()
+	autoReforgeEnabled = false
+	if dynamicButtonTextHandle then
+		dynamicButtonTextHandle:Cancel()
+		dynamicButtonTextHandle = nil
+	end
+	MysticMaestroEnchantingFrameAutoReforgeButton:SetText("Auto Reforge")
+end
+
+
+
+-- Trigger 4 : POLI_AUTO_AUTO_ON, POLI_AUTO_AUTO_OFF
+local function StartAutoAutoReforge()
+	if bagID == nil then
+		bagID = 0
+		slotIndex = 0
+	end
+	if findNextInsignia() then
+		autoAutoEnabled = true
+		RequestReforge()
+	end
+	MysticMaestroEnchantingFrameAutoReforgeButton:SetText("Reforging" .. ThreeDotsString())
+	dynamicAutoButtonTextHandle = Timer.NewTicker(1, function()
+		MysticMaestroEnchantingFrameAutoReforgeButton:SetText("Reforging" .. ThreeDotsString())
+	end)
+end
+
+local function StopAutoAutoReforge()
+	autoAutoEnabled = false
+	if dynamicAutoButtonTextHandle then
+		dynamicAutoButtonTextHandle:Cancel()
+		dynamicAutoButtonTextHandle = nil
+	end
+	MysticMaestroEnchantingFrameAutoReforgeButton:SetText("Auto Reforge")
+end
