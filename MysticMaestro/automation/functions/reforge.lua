94791bac20007756e4e229dcabebd05154b1a9e3
BanditTech <thebbandit@gmail.com>
2023-02-02 11:28:16 -0800

    save previous altar gained xp this will allow the estimation to fill missing information on reload

--- MysticMaestro/automation/functions/reforge.lua
+++ MysticMaestro/automation/functions/reforge.lua
@@ -257,7 +257,15 @@ function MM:ASCENSION_REFORGE_PROGRESS_UPDATE(event, subEvent, xp, level)
 	if not MM.db.realm.AltarXP then MM.db.realm.AltarXP = 0 end
 	if xp == 0 or level == 0 then return end
 	local gained = xp - MM.db.realm.AltarXP
-	if gained == 0 then return end
+	if gained == 0 then
+		if MM.db.realm.prevAltarGained then
+			gained = MM.db.realm.prevAltarGained
+		else
+			return
+		end
+	else
+		MM.db.realm.prevAltarGained = gained
+	end
 	local remaining = AltarLevelRequireXP(level) - xp
 	local levelUP = math.floor(remaining / gained) + 1
 	AltarReforgesText:SetText("Next level in " .. levelUP .. " reforges")
