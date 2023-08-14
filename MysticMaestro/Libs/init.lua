27afc50022f101f427684c6261c8d303fa4b4bf7
Anch <adrianpratt.ap@gmail.com>
2023-08-14 19:59:42 +1200

    Update init.lua

--- MysticMaestro/Libs/init.lua
+++ MysticMaestro/Libs/init.lua
@@ -1,3 +1,2 @@
-﻿local AceAddon = LibStub("AceAddon-3.0")
-MysticMaestro = LibStub("AceAddon-3.0"):NewAddon("MysticMaestro", "AceConsole-3.0", "AceEvent-3.0", "AceTimer-3.0", "AceSerializer-3.0", "AceComm-3.0", "AceHook-3.0", "AceBucket-3.0")
+﻿MysticMaestro = LibStub("AceAddon-3.0"):NewAddon("MysticMaestro", "AceConsole-3.0", "AceEvent-3.0", "AceTimer-3.0", "AceSerializer-3.0", "AceComm-3.0", "AceHook-3.0", "AceBucket-3.0")
 MysticMaestro.dewdrop = AceLibrary("Dewdrop-2.0");
