c14931f4979f75e887d63768162514fa90fd3775
BanditTech <thebbandit@gmail.com>
2023-08-14 19:54:43 -0700

    exclude reforge results when not in reforge loop

--- MysticMaestro/utils/reforge.lua
+++ MysticMaestro/utils/reforge.lua
@@ -253,7 +253,7 @@ local function configConditionMet(currentEnchant)
 end
 
 function MM:MYSTIC_ENCHANT_REFORGE_RESULT(event, result, SpellID)
-	if result ~= "RE_REFORGE_OK" then return end
+	if not autoReforgeEnabled or result ~= "RE_REFORGE_OK" then return end
 	local currentEnchant = C_MysticEnchant.GetEnchantInfoBySpell(SpellID)
 	local result = configConditionMet(currentEnchant)
 	local norunes = configNoRunes()
